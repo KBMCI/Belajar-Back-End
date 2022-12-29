bdfa3ad2efe8fd9850198dd12f2ef88126196ae8
Aldy Imam Wijaya <aldyimam03@gmail.com>
2022-12-29 19:45:29 +0700

    Create 10_typeDeclaration.go

---
+++ Aldy Imam Wijaya/Golang/belajarGolangDasar/10_typeDeclaration.go
@@ -0,0 +1,14 @@
+package main;
+
+import "fmt";
+
+func main() {
+	type nimAldy uint64;
+	type menikah bool;
+
+	var nimMahasiswa nimAldy = 215150700111039;
+	fmt.Println(nimMahasiswa);
+
+	var statusMenikah menikah = true;
+	fmt.Println(statusMenikah);
+};
