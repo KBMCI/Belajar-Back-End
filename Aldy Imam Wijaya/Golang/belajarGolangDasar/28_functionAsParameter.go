f8bc7ae14b5d6e250592b6912efb1a72f2a5bd04
Aldy Imam Wijaya <aldyimam03@gmail.com>
2023-01-01 11:03:11 +0700

    Update 28_functionAsParameter.go

--- Aldy Imam Wijaya/Golang/belajarGolangDasar/28_functionAsParameter.go
+++ Aldy Imam Wijaya/Golang/belajarGolangDasar/28_functionAsParameter.go
@@ -1,5 +1,25 @@
 package main
+import "fmt"
+
+type Filter func(string) string // menggunakan type declaration (ada di bab nomer 10)
+
+func sayHelloWithFilter(nama string, filter Filter) {
+	namaFilter := filter(nama)
+	fmt.Println("Halo ",namaFilter)
+}  
+
+func spamFilter(nama string) string {
+	if nama == "Anjing" {
+		return "***"
+	}else {
+		return nama
+	}
+}
 
 func main() {
-	
+	sayHelloWithFilter("Eko", spamFilter)
+
+	filter := spamFilter
+
+	sayHelloWithFilter("Anjing", filter)
 }
