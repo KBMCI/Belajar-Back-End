fd0397f86a451ab9fa189fc58a295b855bcaadf5
Aldy Imam Wijaya <aldyimam03@gmail.com>
2023-01-01 11:02:58 +0700

    Update 23_functionReturnValue.go

--- Aldy Imam Wijaya/Golang/belajarGolangDasar/23_functionReturnValue.go
+++ Aldy Imam Wijaya/Golang/belajarGolangDasar/23_functionReturnValue.go
@@ -7,12 +7,12 @@ func getHello(nama string, nim int64) string  {
 	if nama == "" {
 		return "Halo Bro"  
 	} else {
-		return "Halo" + nama
+		return "Halo " + nama
 	}
 }
 
 func main() {
-	hasil := getHello("Aldy", 215150700111039);
+	hasil := getHello("Aldy", 215150700111039); // nim tidak akan tercetak karena value yang dikembalikan hanya string
 	fmt.Println(hasil);
 
 	fmt.Println(getHello("", 0))
