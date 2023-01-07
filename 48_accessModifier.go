a5bb2f40ea5918b83e012358af28cca59b88b094
Aldy Imam Wijaya <aldyimam03@gmail.com>
2023-01-07 20:09:05 +0700

    Update 48_accessModifier.go

--- 48_accessModifier.go
+++ 48_accessModifier.go
@@ -6,7 +6,7 @@ import (
 )
  // kalo dijava yang biasanya classnya public, private gitu gitu deh pokoknya
 
-func main() {
+func main49() {
 	fmt.Println(helper.Application) // tidak error
 
 	// fmt.Println(helper.version)
